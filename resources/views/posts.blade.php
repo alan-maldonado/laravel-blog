8dd8a5dc9c2a63e04a9651a0860c39dd00b0f806
Alan Maldonado <alan.m@incfile.com>
2024-02-23 15:13:41 -0600

    Get All Posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,22 +2,9 @@
 <title>My Blog</title>
 <link rel="stylesheet" href="./app.css">
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, soluta sunt excepturi voluptas architecto libero adipisci perferendis dicta suscipit, quod eum explicabo laboriosam. Id ipsa mollitia perferendis placeat optio natus illo quos. Ut non officia perferendis dignissimos fugiat, itaque numquam soluta vel ducimus magni eaque vero ratione aliquam minima placeat debitis optio nostrum praesentium odit voluptatibus culpa quam sit saepe illum! Incidunt vitae vel consequuntur eaque obcaecati enim quisquam minus qui corporis voluptate! Deserunt expedita et nihil voluptate reprehenderit cupiditate tempore, quidem sed fugiat aut obcaecati reiciendis exercitationem corrupti veritatis perferendis quos molestiae quisquam tempora ea a? Sint, vitae iusto.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, soluta sunt excepturi voluptas architecto libero adipisci perferendis dicta suscipit, quod eum explicabo laboriosam. Id ipsa mollitia perferendis placeat optio natus illo quos. Ut non officia perferendis dignissimos fugiat, itaque numquam soluta vel ducimus magni eaque vero ratione aliquam minima placeat debitis optio nostrum praesentium odit voluptatibus culpa quam sit saepe illum! Incidunt vitae vel consequuntur eaque obcaecati enim quisquam minus qui corporis voluptate! Deserunt expedita et nihil voluptate reprehenderit cupiditate tempore, quidem sed fugiat aut obcaecati reiciendis exercitationem corrupti veritatis perferendis quos molestiae quisquam tempora ea a? Sint, vitae iusto.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, soluta sunt excepturi voluptas architecto libero adipisci perferendis dicta suscipit, quod eum explicabo laboriosam. Id ipsa mollitia perferendis placeat optio natus illo quos. Ut non officia perferendis dignissimos fugiat, itaque numquam soluta vel ducimus magni eaque vero ratione aliquam minima placeat debitis optio nostrum praesentium odit voluptatibus culpa quam sit saepe illum! Incidunt vitae vel consequuntur eaque obcaecati enim quisquam minus qui corporis voluptate! Deserunt expedita et nihil voluptate reprehenderit cupiditate tempore, quidem sed fugiat aut obcaecati reiciendis exercitationem corrupti veritatis perferendis quos molestiae quisquam tempora ea a? Sint, vitae iusto.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all(),
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        // dd('File does not exist');
-        // ddd('File does not exist');
-        // abort(404);
-        return redirect('/');
-    }
+    $post = Post::find($slug);
 
-    $post = Cache::remember("post.{$slug}", now()->addSeconds(5), fn() => file_get_contents($path));
-
-    return view('post', ['post' => $post]);
-})-> where('post', '[A-z_\-]+');
+    return view('post', [
+        'post' => $post,
+    ]);
+})->where('post', '[A-z_\-]+');
