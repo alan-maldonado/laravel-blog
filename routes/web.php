378ad59f465d4f098124460374e2649b3086f1e3
Alan Maldonado <alan.m@incfile.com>
2024-02-23 15:55:41 -0600

    Refactor with collections

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +16,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
 
-    $files = File::files(resource_path('posts'));
-    $posts = [];
-
-    foreach ($files as $file) {
-        $document = YamlFrontMatter::parseFile($file);
-        $posts[] = new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug,
-        );
-    }
-    // ddd($posts);
-
     return view('posts', [
-        'posts' => $posts,
+        'posts' => Post::all(),
     ]);
 });
 
