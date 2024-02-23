84a675a3c13b8524df7f741db77e32432466d5e9
Alan Maldonado <alan.m@incfile.com>
2024-02-23 17:38:53 -0600

    Use blade template

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,11 @@
-<!doctype html>
-<title>My Blog</title>
-<link rel="stylesheet" href="./app.css">
-<body>
+@extends('layout')
+
+@section('content')
     @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'mb-6' : ''}}">
+        <article class="{{ $loop->even ? 'mb-6' : '' }}">
             <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{ $post->title}}
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
             <div>
@@ -14,4 +13,4 @@
             </div>
         </article>
     @endforeach
-</body>
+@endsection
