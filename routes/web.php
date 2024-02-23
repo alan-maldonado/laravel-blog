505345465adb2e59133f4ab9bf3d231b7b040d65
Alan Maldonado <alan.m@incfile.com>
2024-02-23 14:41:21 -0600

    Refactor php

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,14 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (! file_exists($path)) {
+    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         // dd('File does not exist');
         // ddd('File does not exist');
         // abort(404);
         return redirect('/');
     }
 
-    $post = Cache::remember("post.{$slug}", now()->addSeconds(5), function () use($path) {
-        var_dump('file_get_contents');
-        return file_get_contents($path);
-    });
+    $post = Cache::remember("post.{$slug}", now()->addSeconds(5), fn() => file_get_contents($path));
 
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => $post]);
 })-> where('post', '[A-z_\-]+');
